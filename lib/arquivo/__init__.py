134fa0880a9815d6e6bd9801033bf71c567809cf
Igor Marques de Figueirêdo <igor.figueiredo@academico.ifpb.edu.br>
2026-06-13 00:22:04 -0300

    salvando

--- lib/arquivo/__init__.py
+++ lib/arquivo/__init__.py
@@ -27,7 +27,7 @@ def lerArquivo(nome):
         for linha in a:
             dado = linha.split(';')
             dado[1] = dado[1].replace('\n','')
-            print(f'{dado[0]:<30}{dado[1]:>3} anos')
+            print(f'{dado[0]:<30}{dado[1]} anos')
     finally:
         a.close()
 
